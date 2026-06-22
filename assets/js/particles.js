0f390784d30a6b91107816fe894b8f0d7d7285f1
Ianpestrella <80919153+Ianpestrella@users.noreply.github.com>
2026-06-22 14:06:51 +0200

    Add particles.js for particle effects

---
+++ assets/js/particles.js
@@ -0,0 +1,51 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": { 
+      "value": 60 
+    },
+    "color": { 
+      "value": "#888888" 
+    },
+    "shape": { 
+      "type": "circle" 
+    },
+    "opacity": { 
+      "value": 0.5 
+    },
+    "size": { 
+      "value": 3 
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#888888",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "none",
+      "out_mode": "out"
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { 
+        "enable": true, 
+        "mode": "grab" 
+      },
+      "onclick": { 
+        "enable": true, 
+        "mode": "push" 
+      }
+    },
+    "modes": {
+      "grab": { 
+        "distance": 140, 
+        "line_linked": { "opacity": 1 } 
+      }
+    }
+  },
+  "retina_detect": true
+});
